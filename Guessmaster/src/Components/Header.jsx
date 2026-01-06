6e42528aec57aa6865f12799179ecbdd401e6912
Somesh Sinha <someshsinha902@gmail.com>
2026-01-06 17:01:19 +0530

    stated with my new react project a word guessing game

---
+++ Guessmaster/src/Components/Header.jsx
@@ -0,0 +1,12 @@
+function Header() {
+  return (
+    <div className="Header">
+      <h1>Assembly Endgame</h1>
+      <p>
+        Guess the correct word in under 8 attempts to keep the programming world
+        safe from Assembly
+      </p>
+    </div>
+  );
+}
+export default Header;
